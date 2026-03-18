1fd93190e9b1e68e6643df78dcedd1e044221688
Devyani Gajapure <145180163+devyanih@users.noreply.github.com>
2026-03-18 12:25:42 +0530

    Time: 0 ms (100%), Space: 44.7 MB (64.52%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,5 +1,4 @@
 class Solution {
-    // reverse function
     public static void reverse(int nums[], int s, int e){
         while(s <= e){
             int temp=nums[s];
@@ -10,29 +9,33 @@ class Solution {
         }
     }
     public void nextPermutation(int[] nums) {
-        int pivot=-1;
+        // find pivot
+        int pivot = -1;
         int n=nums.length;
         for(int i=n-2;i>=0;i--){
-            if(nums[i]< nums[i+1]){
+            if(nums[i]<nums[i+1]){
                 pivot=i;
                 break;
             }
         }
-        if(pivot==-1){
+        if(pivot == -1){
             reverse(nums,0,n-1);
             return;
         }
-        // step 2 finding right most element> pivot
+
+        // right most num > pivot
+
         for(int i=n-1;i>pivot;i--){
             if(nums[i]>nums[pivot]){
+                //swap
                 int temp=nums[i];
                 nums[i]=nums[pivot];
                 nums[pivot]=temp;
                 break;
             }
-
         }
-        // step 3 reverse element from pivot +1
-            reverse(nums,pivot+1,n-1);
+
+        //reverse nums after pivot
+        reverse(nums,pivot+1,n-1); 
     }
 }
